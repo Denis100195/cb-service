71007fe8df5aebe0430bb38273804f7ea2a0327d
Den <den@ja.com>
2024-08-01 11:04:04 +0300

    third commit

--- src/main/java/ru/financial/data/cbservice/client/CbrClient.java
+++ src/main/java/ru/financial/data/cbservice/client/CbrClient.java
@@ -2,8 +2,7 @@ package ru.financial.data.cbservice.client;
 
 
 import org.springframework.stereotype.Service;
-import ru.cbr.web.DailyInfoSoap;
-import ru.cbr.web.DragMetDynamicResponse;
+import ru.cbr.web.*;
 import ru.financial.data.cbservice.config.CbConfig;
 
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -23,7 +22,6 @@ public class CbrClient extends BaseClient{
                 cbConfig.getRead(),
                 getCustomFeatures());
     }
-
     private static XMLGregorianCalendar toXGC(LocalDate date) throws DatatypeConfigurationException {
         XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar();
 
@@ -33,15 +31,34 @@ public class CbrClient extends BaseClient{
         xmlGregorianCalendar.setTime(0,0,0);
         xmlGregorianCalendar.setTimezone(DatatypeConstants.FIELD_UNDEFINED);
         return xmlGregorianCalendar;
-
     }
     public DragMetDynamicResponse.DragMetDynamicResult getDragMetDynamic(LocalDate fromDate, LocalDate toDate) throws DatatypeConfigurationException {
-
         DragMetDynamicResponse.DragMetDynamicResult dragMetDynamicResult = wsClient.dragMetDynamic(
                 CbrClient.toXGC(fromDate),
                 CbrClient.toXGC(toDate)
         );
         return dragMetDynamicResult;
     }
+    public KeyRateResponse.KeyRateResult getKeyRate(LocalDate fromDate, LocalDate toDate) throws DatatypeConfigurationException {
+        KeyRateResponse.KeyRateResult keyRateResult = wsClient.keyRate(
+                CbrClient.toXGC(fromDate),
+                CbrClient.toXGC(toDate)
+    );
+        return keyRateResult;
+    }
+    public GetCursOnDateResponse.GetCursOnDateResult getCursOnDate(LocalDate localDate) throws DatatypeConfigurationException {
+        GetCursOnDateResponse.GetCursOnDateResult cursOnDateResult = wsClient.getCursOnDate(
+                CbrClient.toXGC(localDate)
+        );
+        return cursOnDateResult;
+    }
+    public RuoniaResponse.RuoniaResult getRuonia(LocalDate fromDate, LocalDate toDate) throws DatatypeConfigurationException{
+        RuoniaResponse.RuoniaResult ruoniaResult = wsClient.ruonia(
+                CbrClient.toXGC(fromDate),
+                CbrClient.toXGC(toDate)
+        );
+        return ruoniaResult;
+    }
+
 
 }
--- src/test/java/ru/financial/data/cbservice/client/CbrClientTest.java
+++ src/test/java/ru/financial/data/cbservice/client/CbrClientTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.cbr.web.DragMetDynamicResponse;
+import ru.cbr.web.GetCursOnDateResponse;
+import ru.cbr.web.KeyRateResponse;
+import ru.cbr.web.RuoniaResponse;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import java.time.LocalDate;
@@ -16,11 +19,34 @@ class CbrClientTest {
     CbrClient cbrClient;
 
     @Test
-    void test() throws DatatypeConfigurationException {
+    void test1() throws DatatypeConfigurationException {
         DragMetDynamicResponse.DragMetDynamicResult dragMetDynamic = cbrClient.getDragMetDynamic(
                 LocalDate.of(2020, 1, 11),
                 LocalDate.of(2020, 2, 12)
         );
         Assertions.assertNotNull(dragMetDynamic);
+    }
+    @Test
+    void test2() throws DatatypeConfigurationException{
+        KeyRateResponse.KeyRateResult keyRateResult = cbrClient.getKeyRate(
+                LocalDate.of(2020, 1, 11),
+                LocalDate.of(2020, 2, 12)
+        );
+        Assertions.assertNotNull(keyRateResult);
+    }
+    @Test
+    void test3() throws DatatypeConfigurationException{
+        GetCursOnDateResponse.GetCursOnDateResult cursOnDateResult = cbrClient.getCursOnDate(
+                LocalDate.of(2024, 7, 12)
+        );
+        Assertions.assertNotNull(cursOnDateResult);
+    }
+    @Test
+    void test4() throws DatatypeConfigurationException{
+        RuoniaResponse.RuoniaResult ruoniaResult = cbrClient.getRuonia(
+                LocalDate.of(2020, 1, 11),
+                LocalDate.of(2020, 2, 12)
+        );
+        Assertions.assertNotNull(ruoniaResult);
     }
 }
